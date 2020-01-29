fc0063d5b8dfdb0c39940335314b5b1ab5668e08
Rafi'ul Huda <Rafiulhuda27@gmail.com>
2020-01-29 17:59:03 +0700

    menambahkanframelayoutdanbutton

--- app/src/main/java/com/example/android/fragmentexample/SimpleFragmentOne.java
+++ app/src/main/java/com/example/android/fragmentexample/SimpleFragmentOne.java
@@ -12,43 +12,47 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class SimpleFragmentOne extends Fragment {
 
-    private  static final int YES = 0;
+    private static final int YES = 0;
     private static final int NO = 1;
 
     public SimpleFragmentOne() {
         // Required empty public constructor
     }
-
-
     @Override
-    public View onCreateView(final LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
 
-        // Inflate the layout for this fragment
-        final View rootView = inflater.inflate(R.layout.fragment_simple_fragment_one,container,false);
-        final RadioGroup radioGroup= rootView.findViewById(R.id.radio_group);
-        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                View radioButton = radioGroup.findViewById(checkedId);
-                int index = radioGroup.indexOfChild(radioButton);
-                TextView textView = rootView.findViewById(R.id.fragment_one);
-                switch (index){
-                    case YES:
-                        textView.setText(R.string.yes_message);
-                        break;
-                    case NO:
-                        textView.setText(R.string.no_message);
-                        break;
-                    default:
-                        break;
-                }
-            }
-        });
+        // Inflate the layout for this fragment.
+        final View rootView = inflater.inflate(R.layout.fragment_simple_fragment_one,
+                container, false);
+        final RadioGroup radioGroup = rootView.findViewById(R.id.radio_group);
+
+        radioGroup.setOnCheckedChangeListener(
+                new RadioGroup.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged(RadioGroup group, int checkedId) {
+                        View radioButton = radioGroup.findViewById(checkedId);
+                        int index = radioGroup.indexOfChild(radioButton);
+                        TextView textView =
+                                rootView.findViewById(R.id.fragment_one);
+                        switch (index) {
+                            case YES:
+                                textView.setText(R.string.yes_message);
+                                break;
+                            case NO:
+                                textView.setText(R.string.no_message);
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                });
         return rootView;
+    }
+
+    public static SimpleFragmentOne newInstance() {
+        return new SimpleFragmentOne();
     }
 }
